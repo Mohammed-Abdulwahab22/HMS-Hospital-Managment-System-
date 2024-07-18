aff285dfb13f15a3ac221326628804dc4c3c2556
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2024-07-18 16:43:56 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace HMS_Hospital_Managment_System_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
