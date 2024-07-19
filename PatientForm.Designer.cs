79e0f14adf24e6882eed4596142c10c615e25785
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2024-07-19 09:03:52 +0300

    Add Patient Form

--- PatientForm.Designer.cs
+++ PatientForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HMS_Hospital_Managment_System_
 {
-    partial class DoctorForm
+    partial class PatientForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoctorForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatientForm));
             panel1 = new Panel();
             label2 = new Label();
             pictureBox1 = new PictureBox();
@@ -37,11 +37,15 @@
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox6 = new TextBox();
             dataGridView1 = new DataGridView();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
             button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             label3 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
@@ -58,7 +62,7 @@
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(1047, 122);
-            panel1.TabIndex = 0;
+            panel1.TabIndex = 1;
             // 
             // label2
             // 
@@ -67,10 +71,9 @@
             label2.ForeColor = Color.WhiteSmoke;
             label2.Location = new Point(494, 63);
             label2.Name = "label2";
-            label2.Size = new Size(135, 50);
+            label2.Size = new Size(136, 50);
             label2.TabIndex = 4;
-            label2.Text = "Doctor";
-            label2.Click += label2_Click;
+            label2.Text = "Patient";
             // 
             // pictureBox1
             // 
@@ -98,93 +101,94 @@
             // 
             textBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             textBox1.ForeColor = Color.Purple;
-            textBox1.Location = new Point(54, 165);
+            textBox1.Location = new Point(76, 221);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(214, 30);
-            textBox1.TabIndex = 1;
-            textBox1.Text = "Doctor Id";
+            textBox1.TabIndex = 2;
+            textBox1.Text = "Patient Name";
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             textBox2.ForeColor = Color.Purple;
-            textBox2.Location = new Point(54, 201);
+            textBox2.Location = new Point(76, 185);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(214, 30);
-            textBox2.TabIndex = 2;
-            textBox2.Text = "Doctor Name";
+            textBox2.TabIndex = 3;
+            textBox2.Text = "Patient Id";
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
             textBox3.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             textBox3.ForeColor = Color.Purple;
-            textBox3.Location = new Point(54, 237);
+            textBox3.Location = new Point(76, 257);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(214, 30);
-            textBox3.TabIndex = 3;
-            textBox3.Text = "Years Of Experience";
+            textBox3.TabIndex = 4;
+            textBox3.Text = "Patient Address";
             // 
             // textBox4
             // 
             textBox4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             textBox4.ForeColor = Color.Purple;
-            textBox4.Location = new Point(54, 304);
+            textBox4.Location = new Point(76, 293);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(214, 30);
-            textBox4.TabIndex = 4;
-            textBox4.Text = "Password";
+            textBox4.TabIndex = 5;
+            textBox4.Text = "Patient Phone";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.ForeColor = Color.Purple;
+            textBox5.Location = new Point(76, 329);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(214, 30);
+            textBox5.TabIndex = 6;
+            textBox5.Text = "Patient Age";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Male", "Female" });
+            comboBox1.Location = new Point(76, 365);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(214, 33);
+            comboBox1.TabIndex = 7;
+            comboBox1.Text = "Gender";
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "A+", "O+", "B+", "AB+", "A-", "O-", "B-", "AB-" });
+            comboBox2.Location = new Point(76, 404);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(214, 33);
+            comboBox2.TabIndex = 8;
+            comboBox2.Text = "Blood Group";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox6.ForeColor = Color.Purple;
+            textBox6.Location = new Point(76, 443);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(214, 30);
+            textBox6.TabIndex = 9;
+            textBox6.Text = "Major Disease";
             // 
             // dataGridView1
             // 
             dataGridView1.BackgroundColor = Color.White;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(462, 201);
+            dataGridView1.Location = new Point(461, 185);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.Size = new Size(499, 420);
-            dataGridView1.TabIndex = 5;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.Purple;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.WhiteSmoke;
-            button1.Location = new Point(14, 383);
-            button1.Name = "button1";
-            button1.Size = new Size(129, 54);
-            button1.TabIndex = 6;
-            button1.Text = "Add";
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.Purple;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.ForeColor = Color.WhiteSmoke;
-            button2.Location = new Point(149, 383);
-            button2.Name = "button2";
-            button2.Size = new Size(129, 54);
-            button2.TabIndex = 7;
-            button2.Text = "Update";
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.Purple;
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.WhiteSmoke;
-            button3.Location = new Point(284, 383);
-            button3.Name = "button3";
-            button3.Size = new Size(129, 54);
-            button3.TabIndex = 8;
-            button3.Text = "Delete";
-            button3.UseVisualStyleBackColor = false;
+            dataGridView1.TabIndex = 10;
             // 
             // button4
             // 
@@ -193,12 +197,54 @@
             button4.FlatStyle = FlatStyle.Flat;
             button4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button4.ForeColor = Color.WhiteSmoke;
-            button4.Location = new Point(149, 457);
+            button4.Location = new Point(161, 576);
             button4.Name = "button4";
             button4.Size = new Size(129, 54);
-            button4.TabIndex = 9;
+            button4.TabIndex = 14;
             button4.Text = "Home";
             button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Purple;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.WhiteSmoke;
+            button3.Location = new Point(296, 502);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 54);
+            button3.TabIndex = 13;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Purple;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.WhiteSmoke;
+            button2.Location = new Point(161, 502);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 54);
+            button2.TabIndex = 12;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Purple;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.WhiteSmoke;
+            button1.Location = new Point(26, 502);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 54);
+            button1.TabIndex = 11;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
@@ -206,32 +252,36 @@
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Purple;
-            label3.Location = new Point(589, 145);
+            label3.Location = new Point(591, 132);
             label3.Name = "label3";
-            label3.Size = new Size(216, 50);
-            label3.TabIndex = 16;
-            label3.Text = "Doctors List";
+            label3.Size = new Size(217, 50);
+            label3.TabIndex = 15;
+            label3.Text = "Patients List";
             // 
-            // DoctorForm
+            // PatientForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Gainsboro;
-            ClientSize = new Size(1047, 655);
+            ClientSize = new Size(1047, 650);
             Controls.Add(label3);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
+            Controls.Add(textBox6);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox5);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "DoctorForm";
-            Text = "DoctorForm";
+            Name = "PatientForm";
+            Text = "PatientForm";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -243,18 +293,22 @@
         #endregion
 
         private Panel panel1;
-        private Label label1;
-        private PictureBox pictureBox1;
         private Label label2;
+        private PictureBox pictureBox1;
+        private Label label1;
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
         private TextBox textBox4;
+        private TextBox textBox5;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox6;
         private DataGridView dataGridView1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
         private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
         private Label label3;
     }
 }
